f7b28ccdf39194a6dae44d81ba5a0ac3af81941e
Derek Teed <Derek@stp-er-10-129-62-41.wireless.umn.edu>
2019-06-10 22:01:11 -0500

    Updates

---
+++ precache-manifest.703d597406e9b16b038940446ce20d3a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508e91c50cd774341eb0a9fdb81c4b1f",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "9ade5dbc71875d063e07",
+    "url": "/clickyGame/static/css/main.d6c2a5cb.chunk.css"
+  },
+  {
+    "revision": "9ae1f8cf63ff10a4a540",
+    "url": "/clickyGame/static/js/2.046c6606.chunk.js"
+  },
+  {
+    "revision": "9ade5dbc71875d063e07",
+    "url": "/clickyGame/static/js/main.5de59692.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "fbf198f12fb35d53259cd16c2560ea49",
+    "url": "/clickyGame/static/media/AdmiralAckbar.fbf198f1.jpg"
+  },
+  {
+    "revision": "5b38b53c4c3c6eb23803d3513707493b",
+    "url": "/clickyGame/static/media/BenKenobi.5b38b53c.jpg"
+  },
+  {
+    "revision": "e4f42b0e5967417e32f1917aff3f5dbf",
+    "url": "/clickyGame/static/media/C3PO.e4f42b0e.jpg"
+  },
+  {
+    "revision": "1f3ba6306ae47669c649d67b48b7e803",
+    "url": "/clickyGame/static/media/Chewbacca.1f3ba630.jpg"
+  },
+  {
+    "revision": "7f0e1820fde395f63da48259ea73fe75",
+    "url": "/clickyGame/static/media/DarthSidious.7f0e1820.jpg"
+  },
+  {
+    "revision": "923949c6b39f806573561613ae981c94",
+    "url": "/clickyGame/static/media/DarthVader.923949c6.jpg"
+  },
+  {
+    "revision": "2f7b713c7f8098c22289447ce1c72c51",
+    "url": "/clickyGame/static/media/Finn.2f7b713c.jpeg"
+  },
+  {
+    "revision": "88461fe0776bb130c51981e28c1fecac",
+    "url": "/clickyGame/static/media/HanSolo.88461fe0.jpg"
+  },
+  {
+    "revision": "ad52c2dac25749e2c953d8836c97d755",
+    "url": "/clickyGame/static/media/Jabba-the-hutt.ad52c2da.jpg"
+  },
+  {
+    "revision": "ae3d74aea1c753f01ae051d722d1254d",
+    "url": "/clickyGame/static/media/LukeSkywalker.ae3d74ae.jpg"
+  },
+  {
+    "revision": "04cf1892681dede6a97f4b9080010968",
+    "url": "/clickyGame/static/media/Mace-Windu.04cf1892.jpeg"
+  },
+  {
+    "revision": "549cc3b9e2a0a801d38da739df58b9aa",
+    "url": "/clickyGame/static/media/PadmeAmidala.549cc3b9.jpg"
+  },
+  {
+    "revision": "8250ce60eb952c441811eac26d90afd6",
+    "url": "/clickyGame/static/media/Peter-Cushing.8250ce60.jpeg"
+  },
+  {
+    "revision": "efdc85e131a480ae68f41dcda522a23a",
+    "url": "/clickyGame/static/media/PrincessLeia.efdc85e1.jpg"
+  },
+  {
+    "revision": "eb2c367a6fc45aa54049e1af986e010c",
+    "url": "/clickyGame/static/media/Qui-Gon-Jinn.eb2c367a.jpg"
+  },
+  {
+    "revision": "1747cbc5d8ddeefdd73217ce001905d9",
+    "url": "/clickyGame/static/media/Yoda.1747cbc5.jpg"
+  },
+  {
+    "revision": "80ff69f319daa945a850b4efe621c19a",
+    "url": "/clickyGame/static/media/background.80ff69f3.jpeg"
+  },
+  {
+    "revision": "68a5dd31e75036d1fe433c6b25ac560e",
+    "url": "/clickyGame/static/media/bb8.68a5dd31.jpeg"
+  },
+  {
+    "revision": "b10a332cc94f44fc3b16c04ab7e124d8",
+    "url": "/clickyGame/static/media/kylo-ren.b10a332c.jpeg"
+  },
+  {
+    "revision": "a0dc5fcc2923a5c09104ceda074bd855",
+    "url": "/clickyGame/static/media/rey.a0dc5fcc.jpeg"
+  }
+]);
